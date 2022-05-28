ffe28358e0e0ee49975c0cc716eac33beb278e76
achalogy <achalogy@gmail.com>
2022-05-28 13:16:07 -0500

    button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,16 @@ import CertsComponent from '../components/Home/certs'
 import WelcomeComponent from '../components/Home/welcome'
 import 'aos/dist/aos.css'
 import AOS from 'aos'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
+import githubLogo from '../assets/github.png'
+import fccLogo from '../assets/fcc.png'
+import Image from "next/image";
 
 export default function Home () {
 
   const [isPhone, phone] = useState(false)
+  const [deployed, deploy] = useState(false)
 
   useEffect(() => {
     phone(window.innerWidth < 1000)
@@ -22,7 +28,14 @@ export default function Home () {
       <Head>
         <title>Achalogy</title>
       </Head>
-      {!isPhone &&
+      {isPhone && deployed &&
+        <div className='at-mobile-menu' data-aos="fade-left">
+          <div className="close">
+            <FontAwesomeIcon icon={faX} color="#fff" size='lg' onClick={() => /* deploy(false) */ null} />
+          </div>
+        </div>
+      }
+      {!isPhone ?
         <div className='navbar'>
           <ul>
             <li><a href="#AboutMe">About Me</a></li>
@@ -32,7 +45,23 @@ export default function Home () {
             Contact
           </a>
         </div>
+        :
+        <div className="mobile-menu">
+          <FontAwesomeIcon icon={faBars} color="#9bc2fd" size='2x' onClick={() => /* deploy(true) */ null} />
+        </div>
       }
+      <div className='left-float'>
+        <div className="left-button">
+          <a href="https://github.com/Achalogy" target="_blank">
+            <Image src={githubLogo} width={50} height={50} alt="Github" />
+          </a>
+        </div>
+        <div className="left-button">
+          <a href="https://www.freecodecamp.org/Achalogy" target="_blank">
+            <Image src={fccLogo} width={50} height={34.0136} alt="Github" />
+          </a>
+        </div>
+      </div>
       <WelcomeComponent />
       <AboutMeComponent />
       <CertsComponent />
@@ -43,6 +72,54 @@ export default function Home () {
         </p>
       </footer>
       <style jsx>{`
+
+        .left-float {
+          position: fixed;
+          min-height: 2vw;
+          min-width: 2vw;
+          padding: 0.3vw;
+          background-color: #fff;
+          display: flex;
+          justify-content: space-between;
+          flex-direction: column;
+          border-top-right-radius: 10px;
+          border-bottom-right-radius: 10px;
+        }
+
+        .left-button {
+          background-color: transparent;
+          height: 3vw;
+          width: 3vw;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .close {
+          width: 100%;
+          display: flex;
+          padding: 5vw;
+          justify-content: end;
+          align-items: center;
+        }
+
+        .at-mobile-menu {
+          position: fixed;
+          height: 100vh;
+          width: 80vw;
+          background-color: #05080a;
+          right: 0;
+          z-index: 1;
+        }
+
+        .mobile-menu {
+          width: 100vw;
+          height: 8vh;
+          display: flex;
+          align-items: center;
+          justify-content: end;
+          padding: 0 6vw;
+        }
 
         .navbar ul {
           list-style: none;
@@ -86,6 +163,19 @@ export default function Home () {
         footer p {
           text-align: center;
           margin: 1vh;
+        }
+
+        @media screen and (max-width: 1000px) {
+          .left-float {
+            min-height: 2vh;
+            min-width: 2vh;
+            padding: 0.7vh;
+          }
+          .left-button {
+            height: 3.5vh;
+            width: 3.5vh;
+            margin: 1vw;
+          }
         }
 
       `}</style>
